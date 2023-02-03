1eba386a00da877e99df6dc45080323dcfdbfb3b
jinzelei <2690708525@qq.com>
2023-02-03 17:24:06 +0800

    前台门户启动类

--- Blog/blog-sunset-app/blog-sunset-portal-app/src/main/java/com/jin/blog/sunset/portal/SunsetPortalApplication.java
+++ Blog/blog-sunset-app/blog-sunset-portal-app/src/main/java/com/jin/blog/sunset/portal/SunsetPortalApplication.java
@@ -1,17 +1,20 @@
 package com.jin.blog.sunset.portal;
 
+import com.jin.blog.sunset.base.config.UpdateBeanNameGenerator;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
- * @Description 前台系统启动类
+ * @Description 前台门户启动类
  * @ClassName: SunsetPortalApplication
  * @author: jinzelei
- * @date: 2023/1/13 17:00
+ * @date: 2023/2/2 17:17
  **/
 @SpringBootApplication
-@ComponentScan("com.jin.blog.sunset")
+@ComponentScan(value = "com.jin.blog.sunset",nameGenerator = UpdateBeanNameGenerator.class)
+@MapperScan("com.jin.blog.sunset.core")
 public class SunsetPortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(SunsetPortalApplication.class);

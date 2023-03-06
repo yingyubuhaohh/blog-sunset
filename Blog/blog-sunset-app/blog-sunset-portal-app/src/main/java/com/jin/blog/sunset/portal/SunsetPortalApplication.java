fd87bb8074765be1a69860d56acdb83ed93fcdb4
jinzelei <17885821270@163.com>
2023-03-06 15:52:10 +0800

    开启二级缓存

--- Blog/blog-sunset-app/blog-sunset-portal-app/src/main/java/com/jin/blog/sunset/portal/SunsetPortalApplication.java
+++ Blog/blog-sunset-app/blog-sunset-portal-app/src/main/java/com/jin/blog/sunset/portal/SunsetPortalApplication.java
@@ -4,6 +4,7 @@ import com.jin.blog.sunset.base.config.UpdateBeanNameGenerator;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(value = "com.jin.blog.sunset",nameGenerator = UpdateBeanNameGenerator.class)
 @MapperScan("com.jin.blog.sunset.core")
+@EnableCaching
 public class SunsetPortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(SunsetPortalApplication.class);

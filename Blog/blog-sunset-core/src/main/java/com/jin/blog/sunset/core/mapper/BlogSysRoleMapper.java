38db8f9d2063797447ce9f958de095728a45be43
jinzelei <2690708525@qq.com>
2023-04-16 16:49:27 +0800

    添加二级缓存

--- Blog/blog-sunset-core/src/main/java/com/jin/blog/sunset/core/mapper/BlogSysRoleMapper.java
+++ Blog/blog-sunset-core/src/main/java/com/jin/blog/sunset/core/mapper/BlogSysRoleMapper.java
@@ -2,6 +2,7 @@ package com.jin.blog.sunset.core.mapper;
 
 import com.jin.blog.sunset.core.entity.BlogSysRole;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -13,6 +14,7 @@ import org.apache.ibatis.annotations.Mapper;
  * @since 2023-03-08 14:53:56
  */
 @Mapper
+@CacheNamespace
 public interface BlogSysRoleMapper extends BaseMapper<BlogSysRole> {
 
 }
